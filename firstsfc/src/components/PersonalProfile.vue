911a4730ed749dc0818b735988b4a4a4b43359a7
gbrllqc <gqcalibo@student.apc.edu.ph>
2026-02-13 02:33:48 +0800

    task02fixed

--- firstsfc/src/components/PersonalProfile.vue
+++ firstsfc/src/components/PersonalProfile.vue
@@ -1,42 +1,21 @@
 <template>
   <div class="profile-wrapper">
-    <header>
-      <h1>Personal Profile Web page</h1>
-      <p class="subtitle">About Me</p>
-    </header>
+    <h1>Personal Profile Web page</h1>
+    <p>About Me</p>
 
     <section class="info-content">
       <div class="bio">
         <p><strong>Name:</strong> Gabrielle Calibo</p>
         <p><strong>Nickname:</strong> Gab</p>
-        
-        <div class="education">
-          <strong>Education / Achievements:</strong>
-          <ul>
-            <li>Elementary - University of Perpetual Help System Dalta Molino (2018)</li>
-            <li>High School - St. Mary's Academy Pasay (2022)</li>
-            <li>Senior High School (STEM) - St. Mary's Academy Pasay (2024)</li>
-          </ul>
-        </div>
-
-        <p><strong>Course:</strong> BSIT</p>
-        <p><strong>IT Experience:</strong> [Pending]</p>
-        
-        <div class="hobbies">
-          <strong>Hobbies & Interests:</strong>
-          <p>I like to watch Horror, RomCom, SitCom, Drama, and Crime films. I also enjoy Puzzle Games (though I'm still practicing!).</p>
-        </div>
-
-        <p><strong>Goals In Life / Dream:</strong> To be rich, not only in money but in health too.</p>
+        <p><strong>Education:</strong> BSIT (St. Mary's Academy Pasay)</p>
+        <p><strong>Goals:</strong> To be rich in money and health.</p>
       </div>
 
       <div class="main-image">
-        <p><strong>Featured Image:</strong></p>
         <img src="https://wallpapers.com/images/featured-full/winnie-the-pooh-classic-1egcaz3zkdiirn2q.jpg" alt="pooh" class="featured-img">
       </div>
     </section>
 
-    <h3 style="text-align: center;">Character Gallery</h3>
     <div class="container gallery">
       <div class="gallery-item">
         <img src="https://i.pinimg.com/1200x/4a/07/87/4a07872c8029795ed887b5d9694c6001.jpg" alt="piglet">
@@ -51,5 +30,10 @@
   </div>
 </template>
 
-<script></script>
+<script>
+export default {
+  name: 'PersonalProfile'
+}
+</script>
+
 <style src="./css/personalprofile.css"></style>
