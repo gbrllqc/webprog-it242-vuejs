fb94bd171f6045aad55853b62481e8ac52838f07
gbrllqc <gqcalibo@student.apc.edu.ph>
2026-02-16 13:01:22 +0800

    sana gumana

--- firstsfc/src/components/PersonalProfile.vue
+++ firstsfc/src/components/PersonalProfile.vue
@@ -22,16 +22,16 @@
       <div id="profileCarousel" class="carousel slide shadow-lg profile-carousel themed-img overflow-hidden" data-bs-ride="carousel">
         <div class="carousel-inner">
           <div class="carousel-item active">
-            <img src="@/css//images/me1.jpg" class="d-block w-100" alt="Gabrielle Profile 1">
+            <img src="@/firstsfc/components/css/images/me1.jpg" class="d-block w-100" alt="Gabrielle Profile 1">
           </div>
           <div class="carousel-item">
-            <img src="@/css/images/me2.jpg" class="d-block w-100" alt="Gabrielle Profile 2">
+            <img src="@/firstsfc/components/css/me2.jpg" class="d-block w-100" alt="Gabrielle Profile 2">
           </div>
           <div class="carousel-item">
-            <img src="@/css/images/me3.jpg" class="d-block w-100" alt="Gabrielle Profile 3">
+            <img src="@/firstsfc/components/css/me3.jpg" class="d-block w-100" alt="Gabrielle Profile 3">
           </div>
           <div class="carousel-item">
-            <img src="@/ss/images/me4.jpg" class="d-block w-100" alt="Gabrielle Profile 4">
+            <img src="@/firstsfc/components/css/images/me4.jpg" class="d-block w-100" alt="Gabrielle Profile 4">
           </div>
         </div>
         <button class="carousel-control-prev" type="button" data-bs-target="#profileCarousel" data-bs-slide="prev">
